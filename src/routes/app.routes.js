eeb4aeb51940261a971e8bdd826dfb8580fcab52
LucasRafa22 <lucassolimene22@gmail.com>
2026-06-08 17:36:07 -0300

    feat: proteção de rotas

--- src/routes/app.routes.js
+++ src/routes/app.routes.js
@@ -10,17 +10,67 @@ import EditSensorScreen from '../screens/EditSensorScreen';
 
 const Stack = createNativeStackNavigator();
 
+// 🔐 função de proteção simples
+function isLogged() {
+  return !!global.loggedUser;
+}
+
 export default function Routes() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Register" component={RegisterScreen} />
-        <Stack.Screen name="Dashboard" component={DashboardScreen} />
-        <Stack.Screen name="Sensores" component={SensoresScreen} />
-        <Stack.Screen name="CreateSensor" component={CreateSensorScreen} />
-        <Stack.Screen name="EditSensor" component={EditSensorScreen} />
+        <Stack.Screen
+          name="Dashboard"
+          component={DashboardScreen}
+          options={{
+            headerShown: false
+          }}
+          listeners={({ navigation }) => ({
+            focus: () => {
+              if (!isLogged()) {
+                navigation.replace("Login");
+              }
+            }
+          })}
+        />
+
+        <Stack.Screen
+          name="Sensores"
+          component={SensoresScreen}
+          listeners={({ navigation }) => ({
+            focus: () => {
+              if (!isLogged()) {
+                navigation.replace("Login");
+              }
+            }
+          })}
+        />
+
+        <Stack.Screen
+          name="CreateSensor"
+          component={CreateSensorScreen}
+          listeners={({ navigation }) => ({
+            focus: () => {
+              if (!isLogged()) {
+                navigation.replace("Login");
+              }
+            }
+          })}
+        />
+
+        <Stack.Screen
+          name="EditSensor"
+          component={EditSensorScreen}
+          listeners={({ navigation }) => ({
+            focus: () => {
+              if (!isLogged()) {
+                navigation.replace("Login");
+              }
+            }
+          })}
+        />
 
       </Stack.Navigator>
     </NavigationContainer>
